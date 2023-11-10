c2024f03523614f41641a92f5fea1544924be246
Lucas <lucas.corradini.2003@gmail.com>
2023-11-10 17:15:49 -0300

    Starting the Main

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <template>
-  <HeaderComponent />
+  <HeaderComponent/>
+  <MainComponent/>
 </template>
 
 <script>
 import HeaderComponent from "./components/HeaderComponent.vue";
+import MainComponent from "./components/MainComponent.vue";
 
 export default {
   name: "App",
   components: {
     HeaderComponent,
+    MainComponent
   },
 };
 </script>
---
+++ src/components/MainComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <main>
+
+    </main>
+</template>
+
+<script>
+
+export default {
+  name: "MainComponent",
+};
+
+</script>
+
+<style scoped>
+
+</style>
